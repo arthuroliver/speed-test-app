fd2cb59de14623ba0ae6981978514c03ec647512
arthuroliver <arthur1606@gmail.com>
2022-08-29 18:42:58 -0300

    add delete action to the form

--- app/controllers/admin_list_controller.rb
+++ app/controllers/admin_list_controller.rb
@@ -1,6 +1,6 @@
 class AdminListController < ApplicationController
   before_action :authenticate_admin!
-  before_action :set_admin, only: [:edit, :update]
+  before_action :set_admin, only: [:edit, :update, :destroy]
 
   def index
     @admins = Admin.all
@@ -15,6 +15,11 @@ class AdminListController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @admin.destroy
+    redirect_to admin_list_index_path, notice: "Deleted with success!"
   end
 
   private
